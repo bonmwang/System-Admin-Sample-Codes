853f70a19a389caccc7436abba4579f821c92e9e
bonmwang <98228556+bonmwang@users.noreply.github.com>
2024-10-08 19:25:24 +0300

    inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -1,15 +1,1 @@
-# inventpry_management.py
 
-import json
-
-class Inventory:
-  def __init__(self, filename = 'inventory.json'):
-    self.filename = filename
-    self.load_inventory()
-
-
-  def load_inventory(self):
-
-    try:
-
-        with open(self.filename, 'r') as file
